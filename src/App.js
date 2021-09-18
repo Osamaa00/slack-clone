9db2d7a539e3a6189432a6b5c58a39f865567696
Osama <osamanadeem679@gmail.com>
2021-09-18 16:45:54 +0500

    created chat component and now channels can be added and selected from Sidebar -- added channel will be added to the db

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import './App.css';
 import Header from './components/Header/Header'
 import Sidebar from './components/Sidebar/Sidebar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Chat from './components/Chat/Chat';
 
 function App() {
   return (
     <div className="App">
-    
-      {/* Header component */}
-      <Header />
-      <div className="app__body">
-        {/* Sidebar component */}
-        <Sidebar />
-      </div>
-      {/* React Router - Chat Screen*/}
+      <Router>
+        {/* Header component */}
+        <Header />
+        <div className="app__body">
+          {/* Sidebar component */}
+          <Sidebar />
+          <Switch>
+            <Route path="/room/:roomId">
+              <h1>CHAT SCREEN</h1>
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+        </div>
+        {/* React Router - Chat Screen*/}
+      </Router>
     </div>
   );
 }
--- src/components/Sidebar/SidebarOption/SidebarOption.js
+++ src/components/Sidebar/SidebarOption/SidebarOption.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { useHistory } from "react-router";
+import db from "../../../firebase/firebase";
 import "./SidebarOption.css";
 
-function SidebarOption({ Icon, title }) {
+function SidebarOption({ Icon, title, id, addChannelOption }) {
+
+  // whenever you click a link or go back on a page browser stores
+  // it in a history
+  // so when a user click on a channel we push the next page 
+  // in history
+  const history = useHistory();
+
+  const selectChannel = () => {
+    if (id) {
+      history.push(`/room/${id}`)
+    } else {
+      history.push(title);
+    }
+  };
+
+  const addChannel = () => {
+    const channelName = prompt('Please enter the channel name');
+
+    if ( channelName ) {
+      db.collection('rooms').add({
+        name: channelName,
+      })
+    }
+  }
+
   return (
-    <div className="sidebarOption">
+    <div className="sidebarOption" onClick={ addChannelOption ? addChannel : selectChannel } >
       {Icon && <Icon className="sidebarOption__icon" />}
       {Icon ? (
         <h3>{title}</h3>
